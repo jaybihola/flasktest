9af5e599c9a4a6ac1746c86c1941bc50634b6902
Jay Bihola <jaybih@outlook.com>
2023-07-23 00:45:59 -0400

    implement validations with marshmallow

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["API_VERSION"] = "V1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdeliver.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
 
--- resources/store.py
+++ resources/store.py
@@ -3,20 +3,22 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreUpdateSchema, StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
         except KeyError:
             abort(404, message="Store not found")
 
-    def put(self, store_id):
-        store_data = request.get_json()
-
+    @blp.arguments(StoreUpdateSchema)
+    @blp.response(200, StoreSchema)
+    def put(self, store_data, store_id):
         if "name" not in store_data:
             abort(400, message="Bad request. Please provide fields to update")
 
@@ -34,14 +36,16 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Store not found")
 
+
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
 
-    def post(self):
-        store_data = request.get_json()
-
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
         if "name" not in store_data:
             abort(400, message="Bad Request. Ensure 'name' is included")
 
@@ -52,4 +56,4 @@ class StoreList(MethodView):
         store_id = uuid.uuid4().hex
         store = {**store_data, "id": store_id}
         stores[store_id] = store
-        return store, 201
+        return store
